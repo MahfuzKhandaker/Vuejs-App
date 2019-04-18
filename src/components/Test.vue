1acd07aa990a8820f946459c95ceb71fd43a3bde
MahfuzKhandaker <mahfuzkhandaker.bd@gmail.com>
2019-04-19 00:12:13 +0600

    initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="test">
+        <input type="text" v-model="msg"><br>
+        <p>{{msg}}</p>
+        <button v-on:click="rvMsg">Reverse Message</button>
+    <ul>
+       <li v-for=" item in groceryList" :key="item.text">{{item.text}}</li>
+    </ul>
+    <br>
+    <label> First Name </label> <input type="text" v-model="user.f_name">
+    <br>
+    <label> Last Name </label> <input type="text" v-model="user.l_name">
+    <h3>{{ fullName }}</h3>
+    <h2>{{ msg }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    props: {
+        type: String,
+    },
+    data() {
+        return{
+            msg:'Hello Developer',
+            user: { 
+                f_name: 'Mahfuz',
+                l_name: 'Khandaker'
+                },
+            groceryList: [
+                { text: 'Vegetables'},
+                { text: 'Milk'},
+                { text: 'Fish'},
+                { text: 'Meat'},
+                { text: 'Fruits'}
+            ]
+    }
+    },
+    methods:{
+        rvMsg: function(){
+            this.msg = this.msg.split('').reverse().join('')
+        }
+        },
+        computed: {
+            fullName: function(){
+                return this.user.f_name+ ' ' +this.user.l_name
+            }
+
+        }
+   }
+</script>
+
+<style scoped>
+
+</style>
+
